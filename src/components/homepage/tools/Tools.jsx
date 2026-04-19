e23e25914eff60df3cd6e0ae3809dc1499854019
zakiasultanasuma <zakiasultanasuma@gmail.com>
2026-04-20 04:22:06 +0600

    added new section

--- src/components/homepage/tools/Tools.jsx
+++ src/components/homepage/tools/Tools.jsx
@@ -2,12 +2,19 @@ import React, { use } from 'react';
 import AvailableTools from '../../availableTools/AvailableTools';
 
 const Tools = ({ toolsPromise }) => {
-    console.log(toolsPromise)
+    
     const tools =use(toolsPromise);
-    console.log(tools)
+    
     return (
         <div className='container mx-auto'>
-            tools: {tools.length}
+            <h2 className='text-4xl font-bold mb-5 mt-8 text-center'>Premium Digital Tools</h2>
+            <p className='text-[16px] text-[#627382] leading-6 text-center mb-6'>Choose from our curated collection of premium digital products designed <br /> to boost your productivity and creativity.</p>
+            <div className='flex justify-center mb-10 gap-2'>
+                <button className="btn rounded-full w-[14%]  bg-purple-200 text-white bg-gradient-to-r from-[#4F39F6] to-[#9514FA]">Products</button>
+                <button className="btn rounded-full w-[10%]  bg-purple-200 text-purple-800 ml-4">Cart</button>
+            </div>
+         
+        
             <AvailableTools tools={tools} />
         </div>
     );
--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { TiTick } from 'react-icons/ti';
-import Card from '../ui/Card';
 
-const AvailableTools = ({ tools }) => {
-   
+const Card = ((tool)) => {
     return (
-
         <section className=' grid mx-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
             {tools.map((tool)=>{
-                return  <Card tool={tool}></Card>             
+                return               <div className="card bg-base-100  shadow-sm">
                 <div className='flex justify-end mt-4 mr-4'>
 
                 <button className="btn rounded-full w-[40%]  bg-purple-200 text-purple-800 uppercase">{tool.tag}</button>
@@ -35,7 +31,7 @@ const AvailableTools = ({ tools }) => {
       <button className="btn w-full rounded-full bg-gradient-to-r from-[#4F39F6] to-[#9514FA] text-white">Buy Now</button>
     </div>
   </div>
-
+</div>  
     })}
           
          
@@ -44,4 +40,4 @@ const AvailableTools = ({ tools }) => {
     );
 };
 
-export default AvailableTools;
+export default Card;
